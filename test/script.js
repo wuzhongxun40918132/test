e6c32a400a26b6016ddcd0fd2aea8750354e6d7a
wuzhongxun40918132 <148925527+wuzhongxun40918132@users.noreply.github.com>
2024-01-03 10:22:06 +0800

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    // 功能1
+    // 在這裡加入顯示學號、姓名、大頭照的程式碼
+
+    // 功能2
+    $("#btnFontColor").click(function() {
+        // 在這裡加入調整字體顏色的程式碼
+    });
+
+    // 功能3
+    $("#btnBorderStyle").click(function() {
+        // 在這裡加入調整大頭照樣式的程式碼
+    });
+
+    // 功能4
+    $("#btnChangeImage").click(function() {
+        // 在這裡加入換圖的程式碼
+    });
+
+    // 功能5
+    $("#btnFade").click(function() {
+        // 在這裡加入淡入淡出的程式碼
+    });
+
+    // 功能6
+    $("#linkHTML").attr("href", "path/to/your/html/file.html");
+    $("#linkJS").attr("href", "path/to/your/js/file.js");
+});
